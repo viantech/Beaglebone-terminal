fcb228595cbfd239a5abbbf59ea8023c2931af32
viantech <quang.bon128@gmail.com>
2016-07-13 17:59:42 +0700

    "Fix signal handler and keyboard"

--- src/Linux_Zigbee.cpp
+++ src/Linux_Zigbee.cpp
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <sys/signal.h>
 #include <sys/types.h>
-
+#include <sys/ioctl.h>
 #include <sstream>
 #include <string.h>
 #include <iostream>
@@ -21,9 +21,11 @@
 #define BAUDRATE B115200
 //using namespace BlackLib;
 using namespace std;
-
+void sendFile();
+int readKeyboard(char *buffer);
 void signal_handler_IO (int status); 	// signal hanler
 volatile int _wait_flag_ACK = 0;					// TRUE while no signal received
+volatile int _flag_OK = 0;
 int fd;
 void Write_Uart (const void *mess, int port)
 {
@@ -36,6 +38,7 @@ void Write_Uart (const void *mess, int port)
 	//tcflush(port, TCOFLUSH);
 	//sleep((len + 1 + 25) * 100);
 }
+
 bool Contains(const char* MyChar, const char* Search)
 {
 	char *cp = (char *)MyChar;
@@ -55,46 +58,13 @@ bool Contains(const char* MyChar, const char* Search)
 	return false;
 }
 
-int main(){
-
-	struct termios newtio;
-	__sigset_t mask;
-	mask.__val[_SIGSET_NWORDS] = {0};
-	struct sigaction saio; // definition of signal action
-
-    fd = open(DEVICE, O_RDWR | O_NOCTTY | O_NONBLOCK);
-    if (fd < 0)
-    {
-    	perror("open_port: Unable to open /dev/ttyUSB0 \n");
-    	exit(1);
-    }
-    /* install the signal handler before making the device asynchronous*/
-
-    saio.sa_handler = signal_handler_IO;
-    saio.sa_mask = mask;
-    saio.sa_flags = 0;
-    saio.sa_restorer = NULL;
-    sigaction(SIGIO, &saio, NULL);
-    /* allow the process to receive SIGIO */
-    fcntl(fd, F_SETOWN, getpid());
-    /* Make the file descriptor asynchronous */
-    fcntl(fd, F_SETFL, FNDELAY | FASYNC);
-
-    /* set new port settings for canonical input processing */
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD ;
-    newtio.c_iflag = IGNPAR | ICRNL;
-    newtio.c_oflag = 0;
-    newtio.c_lflag = ICANON;
-    newtio.c_cc[VMIN]=1;
-    newtio.c_cc[VTIME]= 5;
-
-    tcflush(fd, TCIOFLUSH);
-    tcsetattr(fd,TCSANOW,&newtio);
+void sendFile()
+{
     /*File handler*/
     FILE *file_ptr;
     size_t flen = 0;
     char* line = NULL;
-    file_ptr = fopen ("/home/quangdo/Downloads/Intelhex/helllo.hex", "r");
+    file_ptr = fopen ("/home/quangdo/Downloads/Intelhex/demo.hex", "r");
     if (file_ptr == NULL) exit(EXIT_FAILURE);
 
 //    char* pre_com[10] = "AT+SCAST:";
@@ -113,18 +83,15 @@ int main(){
     {
     	strcat(line, "\r"); //watch out
     	memcpy(&send_com[9], line, strlen(line)+1);
-	//printf("fu: %02x %02x :ck\n", send_com[strlen(send_com) - 2], send_com[strlen(send_com)-1]);
-//    	strcat(send_com, line);
-//    	cout  << send_com;
-    	Write_Uart(send_com, fd);
+    	//Write_Uart(send_com, fd);
+    	write(fd, send_com, strlen(send_com) + 1);
     	while (_wait_flag_ACK == 0);
-    	_wait_flag_ACK = 0;
+    		_wait_flag_ACK = 0;
     	/*char commandChar[20] = "";
     	readstr = "";
     	string mystr = "";
     	printf (".\n");
     	usleep(100000);
-
     	cout << "Send: ";
     	getline(cin, mystr);
     	for (int i=0;;i++)
@@ -151,11 +118,109 @@ int main(){
     	}*/
     }
     fclose(file_ptr);
-    //if (line)  free(line);
+    if (line)  free(line);
+}
+
+int main(){
+	int nread = 0;
+	char mess[100];
+	struct termios newtio;
+	__sigset_t mask;
+	mask.__val[_SIGSET_NWORDS] = {0};
+	struct sigaction saio; // definition of signal action
+
+    fd = open(DEVICE, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (fd < 0)
+    {
+    	perror("open_port: Unable to open /dev/ttyUSB0 \n");
+    	exit(1);
+    }
+    /* install the signal handler before making the device asynchronous*/
+
+    saio.sa_handler = signal_handler_IO;
+    saio.sa_mask = mask;
+    saio.sa_flags = SA_RESTART;
+    saio.sa_restorer = NULL;
+    sigaction(SIGIO, &saio, NULL);
+    /* allow the process to receive SIGIO */
+    fcntl(fd, F_SETOWN, getpid());
+    /* Make the file descriptor asynchronous */
+    fcntl(fd, F_SETFL, FNDELAY | FASYNC);
+
+    /* set new port settings for canonical input processing */
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD ;
+    newtio.c_iflag = IGNPAR | ICRNL;
+    newtio.c_oflag = 0;
+    newtio.c_lflag = ICANON;
+    newtio.c_cc[VMIN]=1;
+    newtio.c_cc[VTIME]= 5;
+
+    tcflush(fd, TCIOFLUSH);
+    tcsetattr(fd,TCSANOW,&newtio);
+    while(1)
+    {
+    	nread = readKeyboard(mess);
+    	if (nread > 0) {
+//    		printf("Send:");
+//    		fgets(mess, 100, stdin);
+    		if (strstr(mess, "file") != NULL)
+    			sendFile();
+    		if (strstr(mess, "at") != NULL)
+    		{
+//    			mess[strlen(mess) - 1] = '\0';
+    			mess[strlen(mess) - 1] = 0x0D;
+    			write(fd, mess, strlen(mess) + 1);
+//    			while(_flag_OK == 0);
+//    			_flag_OK = 0;
+    		}
+    		nread = 0;
+    	}
+    }
 
     close(fd);
     return 0;
 }
+
+int readKeyboard(char *buffer)
+{
+	//char buffer[128];
+	int result, nread = 0;
+	fd_set inputs, testfds;
+	struct timeval timeout;
+	FD_ZERO (&inputs);
+	FD_SET(0, &inputs);
+	testfds = inputs;
+	timeout.tv_sec = 3;
+	timeout.tv_usec = 500000;
+	result = select(FD_SETSIZE, &testfds, (fd_set*)0, (fd_set*)0, &timeout);
+
+	switch(result)
+	{
+		case 0:
+			//printf("timeout \n");
+			break;
+		case -1:
+			//perror("select error");
+			return -1;
+			//break;
+		default:
+			if(FD_ISSET(0, &testfds))
+			{
+				ioctl(0, FIONREAD, &nread);
+				if(nread == 0)
+				{
+					printf("keyboard done \n");
+					return -1;
+				}
+				nread = read(0, buffer, nread);
+				buffer[nread] = 0;
+				//printf("read %d from keyboard: %s \n", nread, buffer);
+			}
+			break;
+	}
+	return nread;
+}
+
   /***************************************************************************
   * signal handler. sets wait_flag to FALSE, to indicate above loop that     *
   * characters have been received.                                           *
@@ -163,7 +228,7 @@ int main(){
 
   void signal_handler_IO (int status)
   {
-      int i, nbytes;
+      int nbytes = 0;
       char buffer[100] = {0}, *bufptr = NULL;
       if(status != SIGIO) return;
 
@@ -175,20 +240,19 @@ int main(){
           bufptr += nbytes;
           if(nbytes > 4)
           {
-              if (*(bufptr - 1) == '\n' || *(bufptr - 1) == '\r')
+              if (*(bufptr - 2) == 0x0D && *(bufptr - 1) == 0x0A)
                       break;
           }
 
       }
-      if (nbytes < 4) return;
       /*nul terminate the string and see if we got an OK response */
       //printf("signal_handler_IO: signum = %d\r\n", signum);
       *bufptr = '\0';
       bufptr = buffer;
       cout << "Received: " << bufptr << endl;
 
-//    if(strstr(buffer, "ACK") != NULL)
+      if (Contains(buffer, "OK") == true)
+    	  _flag_OK = 1;
       if(Contains(buffer, "ACK") == true)
       	  _wait_flag_ACK = 1;
-  }
-
+}
